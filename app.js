f124028a5bfd4f8b31e9b2a03c8b3145e162ea65
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-02-07 16:32:59 -0500

    updating yml

--- app.js
+++ app.js
@@ -7,40 +7,31 @@ const yaml = require('yamljs');
 const connectDB = require('./config/db');
 const connectRedis = require('./config/redis');
 const emailRoutes = require('./routes/emailRoutes');
-const { Kafka } = require('kafkajs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const corsOptions = {
-    origin: '*',  // Permite todos los orígenes
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
     credentials: true,
     optionsSuccessStatus: 200
   };
-app.use(cors(corsOptions)); // Habilitar CORS
+app.use(cors(corsOptions));
 
-// Conexión a MongoDB
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', service: 'auth-pass-code-sender' });
+  });
+  
 connectDB();
 
-// Conexión a Redis
 const redisClient = connectRedis();
 
-// Configuración de Kafka
-const kafka = new Kafka({
-    clientId: 'password-recovery-service',
-    brokers: [process.env.KAFKA_BROKER]
-});
-const producer = kafka.producer();
-producer.connect().catch(err => console.error('Error connecting to Kafka:', err));
-
-// Configuración de Swagger
 const swaggerDocument = yaml.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Rutas
-app.use('/email', emailRoutes(redisClient, producer));
+app.use('/email', emailRoutes(redisClient));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,16 @@
 const redis = require('redis');
 
 const connectRedis = () => {
-    const redisClient = redis.createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT });
+    const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+    
+    redisClient.on('connect', () => {
+        console.log('Successfully connected to Redis');
+    });
+
     redisClient.on('error', (err) => {
         console.log('Redis error: ', err);
     });
+    
     return redisClient;
 };
 
